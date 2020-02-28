3b74fb4f25cfd2bdf8b93c47d837da642c1e806d
Faisal Ali <fai28683@gmail.com>
2020-02-28 11:20:33 +0000

    Upper case (#15)
    * fix(GetConstraintsPertab): Also work with non-lowercase column names
    
    Possibly closes #6
    
    * Fix for upper case columns
    
    + If the table has upper case columns then it fails , this fixes the issues
    + Upgrade the version of the mock to 2.2
    
    * Upper case column constraints
    
    If the constraints are on upper case column then it fails, this adds fixes the issue with cleanup of upper case constraints
    
    * Update README.md
    
    * Limit the max columns
    
    Postgres can only accomdate max of 1600 columns on the table, so check and fail if the users request to add more than the limiit
    
    * Update the README with a clearer instructions
    
    * Fix constriants 2nd time
    
    + During the constriants recreation , it might fail when there is FK and Primary on the column
    + This fix, checks if the PK creation failed, if yes go ahead and remove some of the rows that violates them
    + and then recreate the constraint again.
    
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>

--- sql.go
+++ sql.go
@@ -244,9 +244,11 @@ func GetPGConstraintDDL(conntype string) []DBConstraints {
 	Debugf("Extracting the DDL of the %s constraints", conntype)
 	var result []DBConstraints
 	query := `
-SELECT n.nspname 
-       || '.' 
-       || c.relname                                   tablename, 
+SELECT '"' 
+       || n.nspname 
+       || '"."' 
+       || c.relname 
+       || '"'                                         tablename, 
        con.conname                                    constraintname, 
        pg_catalog.Pg_get_constraintdef(con.oid, true) constraintKey 
 FROM   pg_catalog.pg_class c, 
@@ -277,10 +279,12 @@ func GetPGIndexDDL() []DBIndex {
 	Debugf("Extracting the unique indexes")
 	var result []DBIndex
 	query := `
-SELECT schemaname 
-       || '.' 
-       || tablename tablename, 
-       indexdef     indexdef 
+SELECT '"' 
+       || schemaname 
+       || '"."' 
+       || tablename 
+       || '"'   tablename, 
+       indexdef indexdef 
 FROM   pg_indexes 
 WHERE  schemaname IN (SELECT nspname 
                       FROM   pg_namespace 
@@ -545,4 +549,26 @@ WHERE  %[2]s = '%[6]s'
 		Debugf("query: %s", query)
 		Fatalf("Error when updating the foreign key for table %s, err: %v", key.Table, err)
 	}
+}
+
+// Delete the violating key
+func deleteViolatingConstraintKeys(tab string, column string) error {
+	Debugf("Deleting the rows the table that violates the constraints: %s:(%s)", tab, column )
+	query := `
+DELETE 
+FROM   %[1]s 
+WHERE  ( 
+              %[2]s) IN 
+       ( 
+                SELECT   %[2]s 
+                FROM     %[1]s 
+                GROUP BY %[2]s 
+                HAVING   count(*) > 1);
+`
+	query = fmt.Sprintf(query, tab, column)
+	_, err := ExecuteDB(query)
+	if err != nil {
+		return err
+	}
+	return nil
 }
