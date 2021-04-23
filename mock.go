e8939fb0c2e03d5ca9c1f2b6e15a91f5b43ef9fa
Faisal Ali <fai28683@gmail.com>
2021-04-23 09:19:08 +0100

    bump the version of mock-data

--- mock.go
+++ mock.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	programName        = "mock"
-	programVersion     = "v2.8"
+	programVersion     = "v2.9"
 	ExecutionTimestamp = TimeNow()
 	Path               = fmt.Sprintf("%s/%s/%s", os.Getenv("HOME"), programName, ExecutionTimestamp)
 )
